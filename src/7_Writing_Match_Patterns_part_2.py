8f933f35d3bc949805853531e72ae2f00d3a0e44
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-21 12:27:39 +0530

    Solved 'Writing match patterns, part 2'

---
+++ src/7_Writing_Match_Patterns_part_2.py
@@ -0,0 +1,31 @@
+"""
+  Write one pattern that only matches forms of “download” (tokens with the lemma “download”),
+  followed by a token with the part-of-speech tag "PROPN" (proper noun).
+"""
+import spacy
+from spacy.matcher import Matcher
+
+nlp = spacy.load("en_core_web_sm")
+matcher = Matcher(nlp.vocab)
+
+doc = nlp(
+  "i downloaded Fortnite on my laptop and can't open the game at all. Help? "
+  "so when I was downloading Minecraft, I got the Windows version where it "
+  "is the '.zip' folder and I used the default program to unpack it... do "
+  "I also need to download Winzip?"
+)
+
+# Write a pattern that matches a form of "download" plus proper noun
+pattern = [
+  { "LEMMA": "download" },
+  { "POS": "PROPN" }
+]
+
+# Add the pattern to the matcher and apply the matcher to the doc
+matcher.add("DOWNLOAD_THINGS_PATTERN", [pattern])
+matches = matcher(doc)
+print("Total matches found:", len(matches))
+
+# Iterate over the matches and print the span text
+for match_id, start, end in matches:
+  print("Match found:", doc[start:end].text)
