01b5c2027b3ca138be1f6dd7623f505f4149236f
kashifulhaque <haque.kashiful7@gmail.com>
2023-11-21 12:01:02 +0530

    Loading pipelines

--- src/4_Loading_Pipelines.py
+++ src/4_Loading_Pipelines.py
@@ -4,3 +4,9 @@
 """
 import spacy
 nlp = spacy.load("en_core_web_sm")
+
+text = "It’s official: Apple is the first U.S. public company to reach a $1 trillion market value"
+
+doc = nlp(text)
+
+print(doc.text)
